18679bb3ea37ddcb2adcfe04ebcac6d0aa6f13f7
Arman Nikoyan <armannikoyan04@icloud.com>
2025-05-01 00:39:53 +0400

    done: ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,9 +6,11 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 18:22:52 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/04/30 18:35:00 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/05/01 00:01:03 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #include <iostream>
 
